5d8ec42fdc6f99feb4d15b4b670e14cdfd59ede6
Radha Krishan <radhakrishan81217@gmail.com>
2026-06-12 05:50:01 +0530

    Add files via upload

---
+++ operaterop.java
@@ -0,0 +1,15 @@
+public class operaterop {
+   public operaterop() {
+   }
+
+   public static void main(String[] var0) {
+    int a = 30;
+    int b = 20;
+
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a*b);
+      System.out.println(a/b);
+
+   }
+}
