aeeeb27631c7b98c2fdbc3a44b25d6728264623f
Radha Krishan <radhakrishan81217@gmail.com>
2026-06-17 06:40:23 +0530

    Update evenodd.java

--- evenodd.java
+++ evenodd.java
@@ -1,34 +1,35 @@
+// Import the Scanner class to take input from the keyboard
+import java.util.Scanner;
+
+// Create a class named evenodd
 public class evenodd {
-    public evenodd() {
-    }
-    
-    public static void main(String[] var0) {
-     int a = 30;
-    
-        if(a%2==0){
-          System.out.println("Even number");
-        }
-        else{
-          System.out.println("Odd number");
-        }
-    }
 
+    // The main() method is the starting point of the Java program
+    public static void main(String[] args) {
 
-    
+        // Create a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+
+        // Ask the user to enter a number
+        System.out.print("Enter a number: ");
+
+        // Store the entered number in the variable 'num'
+        int num = sc.nextInt();
+
+        // Check whether the number is divisible by 2
+        // If the remainder is 0, the number is even
+        if (num % 2 == 0) {
+
+            // Print that the number is even
+            System.out.println(num + " is Even");
+
+        } else {
+
+            // If the remainder is not 0, the number is odd
+            System.out.println(num + " is Odd");
+        }
+
+        // Close the Scanner object to free system resources
+        sc.close();
+    }
 }
-// import java.util.Scanner;
-
-// public class EvenOdd {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-
-//         System.out.print("Enter a number: ");
-//         int num = sc.nextInt();
-
-//         if(num % 2 == 0) {
-//             System.out.println(num + " is Even");
-//         } else {
-//             System.out.println(num + " is Odd");
-//         }
-//     }
-// }
