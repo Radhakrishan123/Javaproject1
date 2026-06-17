f3b846e29a62c2ae0feee344d3ee5411a56c2dce
Radha Krishan <radhakrishan81217@gmail.com>
2026-06-17 06:34:30 +0530

    Update Pyramidpattern.java

--- Pyramidpattern.java
+++ Pyramidpattern.java
@@ -1,50 +1,39 @@
-public class Pyramidpattern {
+// Import the Scanner class to take input from the keyboard
+import java.util.Scanner;
+
+// Create a class named NumberTriangle
+public class Numbertriangle {
+
+    // The main() method is the starting point of the Java program
     public static void main(String[] args) {
 
-        int n = 5;
+        // Create a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
 
-        for(int i=1;i<=n;i++) {
+        // Ask the user to enter the number of rows
+        System.out.print("Enter the number of rows: ");
 
-            for(int j=1;j<=n-i;j++) {
-                System.out.print(" ");
+        // Store the entered value in the variable 'n'
+        int n = sc.nextInt();
+
+        // Outer loop controls the number of rows
+        // It starts from row 1 and continues until row n
+        for (int i = 1; i <= n; i++) {
+
+            // Inner loop prints numbers in each row
+            // It starts from 1 and prints up to the current row number
+            for (int j = 1; j <= i; j++) {
+
+                // Print the value of j without moving to the next line
+                System.out.print(j);
             }
 
-            for(int j=1;j<=2*i-1;j++) {
-                System.out.print("*");
-            }
-
+            // After printing one complete row,
+            // move the cursor to the next line
             System.out.println();
         }
+
+        // Close the Scanner object to free system resources
+        sc.close();
     }
 }
-    
-//import java.util.Scanner;
-
-// public class PyramidPattern {
-
-//     public static void main(String[] args) {
-
-//         Scanner sc = new Scanner(System.in);
-
-//         System.out.print("Enter the number of rows: ");
-//         int n = sc.nextInt();
-
-//         for (int i = 1; i <= n; i++) {
-
-//             // Print spaces
-//             for (int j = 1; j <= n - i; j++) {
-//                 System.out.print(" ");
-//             }
-
-//             // Print stars
-//             for (int j = 1; j <= 2 * i - 1; j++) {
-//                 System.out.print("*");
-//             }
-
-//             // Move to next line
-//             System.out.println();
-//         }
-
-//         sc.close();
-//     }
-// }
